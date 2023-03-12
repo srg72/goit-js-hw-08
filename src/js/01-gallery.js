32ba5b702a24fe86d6da1930f8c25bd93a3aea35
Victor Serhiienko <107862956+srg72@users.noreply.github.com>
2023-03-12 08:54:16 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const divEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+divEl.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.5,
+});
